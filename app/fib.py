b51579cc7bde6863bd7fef06bd1fa65414afab47
seking03 <122938541+seking03@users.noreply.github.com>
2023-10-25 14:27:33 -0400

    Update fib.py fixed syntax error no : for if

--- app/fib.py
+++ app/fib.py
@@ -5,7 +5,7 @@ The zeroth number in the fibonacci sequence is 0. The first number is 1
 Negative numbers should return None
 """
 def fibonacci(position):
-  if(position == 0)
+  if(position == 0):
     return 0
   if(position < 0):
     raise ValueError("Invalid input")
